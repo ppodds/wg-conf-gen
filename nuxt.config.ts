7e0b58df63c55d63a5a506e1337d6b74588b3dfb
ppodds <oscar20020629@gmail.com>
2025-06-04 14:15:58 +0000

    chore(deps): update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2025-06-04',
   css: ["vuetify/styles", "@mdi/font/css/materialdesignicons.css"],
   build: {
     transpile: ["vuetify"],
